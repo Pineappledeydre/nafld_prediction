c2e1a7cb90613000c7d8eef9e3e9eccd98afaa00
Daria Khokhlacheva <89531828+Pineappledeydre@users.noreply.github.com>
2024-10-21 23:56:51 +0530

    Update health_prediction_app.py

--- health_prediction_app.py
+++ health_prediction_app.py
@@ -2692,7 +2692,7 @@ with col2:
         ax.spines['bottom'].set_linewidth(1.5)
         ax.set_xlim([-0.5, 1.5])
         ax.grid(True, axis='x', linestyle='--', alpha=0.6)
-        ax.legend(loc='lower left', fontsize=10)
+        ax.legend(['Норма', 'Вы'],loc='lower left', fontsize=10)
         
         st.pyplot(fig)
 
